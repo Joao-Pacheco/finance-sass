ecd720ff816f026325dc277b1d7ac31aaf7622f3
Joao Pacheco <juhkittopacheco@gmail.com>
2025-03-20 23:52:01 -0300

    feat:login complet

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      this is an authenticated dashboard
+      <UserButton />
     </main>
   );
 }
