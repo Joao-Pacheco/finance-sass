b7d6b49cf699395fecd418837dec07d3948a7332
Joao Pacheco <juhkittopacheco@gmail.com>
2025-04-04 00:24:53 -0300

    feat: handler errors

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,10 +2,18 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+  return c.json({ error: "Internal Error" }, 500);
+});
 
 app.get("/hello", clerkMiddleware(), (c) => {
   const auth = getAuth(c);
